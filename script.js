f4b0ef77c355659e302185f7ddcb9ab27d83af4a
unknown <daria.lantyugova@mail.ru>
2023-04-18 23:07:14 +0300

    1st version (without 2nd array of top-films

---
+++ script.js
@@ -0,0 +1,85 @@
+const directors = [
+    {
+      name: 'Стивен Спилберг',
+      career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
+      films: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
+      top_rated_film: 'Список Шиндлера'
+    },
+    {
+      name: 'Кристофер Нолан',
+      career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
+      films: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
+      top_rated_film: 'Начало'
+    },
+    {
+      name: 'Мартин МакДона',
+      career: 'Сценарист, Режиссёр, Продюсер',
+      films: 'https://www.film.ru/person/martin-makdonah',
+      top_rated_film: 'Три билборда на границе Эббинга, Миссури'
+    },
+    {
+      name: 'Алексей Балабанов',
+      career: 'Режиссёр, Сценарист, Актёр, Продюсер',
+      films: 'https://www.film.ru/person/aleksey-balabanov',
+      top_rated_film: 'Брат'
+    },
+    {
+      name: 'Питер Фаррелли',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+      films: 'https://www.film.ru/person/piter-farrelli',
+      top_rated_film: 'Зелёная книга'
+    },
+    {
+      name: 'Юрий Быков',
+      career: 'Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер',
+      films: 'https://www.film.ru/person/yuriy-bykov',
+      top_rated_film: 'Дурак'
+    },
+    {
+      name: 'Жан-Марк Валле',
+      career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
+      films: 'https://www.film.ru/person/zhan-mark-valle',
+      top_rated_film: 'Далласский клуб покупателей'
+    },
+  ];
+
+// забираем элементы html-файла в переменные
+  const directorElement = document.querySelector('.director__item');
+//   const directorName = document.querySelector('.director__name');
+//   const directorCareer = document.querySelector('.director__career');
+//   const directorFilms = document.querySelector('.director__films');
+
+  
+// перебираем массив
+directors.forEach (function (item) {
+    directorElement.innerHTML +=
+    `
+    <p class="director__name">${item.name}</p>
+    <p class="director__career">${item.career}</p>
+    <p class="director__films"><a class="director__film-link"  href="${item.films}" target="_blank">Фильмография</a></p>`
+})
+
+//   directorElement.innerHTML +=
+//   `<p class="directors__name">${showDirectorName()}</p>
+//   <p class="directors__career">${showDirectorCareer()}</p>`;
+
+
+
+
+//   //   вывод name
+//   let showDirectorName = () => {
+//     for (let director of directors)
+//    return director.name;
+//   }
+
+//   //   вывод career
+//   let showDirectorCareer = () => {
+//     for (let director of directors)
+//    return director.career;
+//   }
+
+//     //   вывод films
+//     let showDirectorFilms = () => {
+//         for (let director of directors)
+//        return director.films;
+//       }
