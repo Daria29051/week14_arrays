781092d56302730ac0e2dbea5c677cdf8841f62f
unknown <daria.lantyugova@mail.ru>
2023-04-19 15:38:23 +0300

    extra task (1st version)

---
+++ extra task/script.js
@@ -0,0 +1,20 @@
+const arr1 = [1, 2];
+const arr2 = [2, 3];
+const arr3 = ['a', 'b'];
+const arr4 = ['b', 'c'];
+const arr5 = ['b', 'e', 'c'];
+const arr6 = ['b', 'b', 'e'];
+const arr7 = ['b', 'c', 'e'];
+const arr8 = ['b', 'e', 'c'];
+
+
+const intersection = (...arrays) => {
+    let result = arrays[0].filter(el => arrays[1].includes(el))
+    if (arrays.length > 2) intersection(result, ...arrays.slice(2))
+    return [...new Set(result)];
+  }
+
+
+   console.log(intersection(arr1, arr2)) // [2]
+   console.log(intersection(arr3, arr4, arr5)) // ['b']
+   console.log(intersection(arr6, arr7, arr8)) // ['b', 'e']
